d775dcfedd1a0102e897a052fb59340df2a6b46f
Thomas J <tjoy01550@gmail.com>
2021-07-24 12:37:51 +0530

    final

--- forms/src/App.js
+++ forms/src/App.js
@@ -1,13 +1,18 @@
 import React, { useState } from 'react';
 import "./App.css";
-import { Button,  Dropdown , Form , FormField  } from "semantic-ui-react";
+import { Button,  Dropdown , Form , FormField, Icon  } from "semantic-ui-react";
 import { FormControl, FormLabel, RadioGroup, Radio, FormControlLabel } from "@material-ui/core";
 
 function App() {
-  const [name, setName] = useState('');
-  const [val, setVal] = useState('');
-  const [val1, setVal1] = useState('');
-  const [val2, setVal2] = useState('');
+  const [values, setValues] = useState({
+    name: '', val1: '', val2: '', val3: '' 
+  });
+
+  const set = name => {
+    return ({ target: { value }}) => {
+      setValues(oldValues => ({...oldValues, [name]: value}));
+    }
+  }
  
   const optionValues = [
     
@@ -42,25 +47,22 @@ function App() {
       value:'Sci-Fi'
     }
   ]
-  const handleSubmit = (e) => {
-    e.preventDefault()
-  }
 
   return (
     <div className="app">
       <div className="app_header">
         <h2>HELP US IN THIS SURVEY</h2>
-        <h3>Hello { name }</h3>
+        <h3>Hello { values.name }</h3>
       </div>
       <div className='body'>
-          <Form className="form" onSubmit={handleSubmit}>
+          <Form className="form">
             <FormField className="wrapper">
                 <input 
                   type="input"
                   className="textField"
-                  value={name}
+                  value={values.name}
                   placeholder=" "
-                  onChange={e => setName(e.target.value)}
+                  onChange={set('name')}
                 />
                   <span className="placeholder">Name</span>
             </FormField>
@@ -72,7 +74,7 @@ function App() {
             <FormField className="wrapper">
             <FormControl>
               <FormLabel>Do you like watching Movies in your free time ?</FormLabel>
-              <RadioGroup  name="options" value={val1} onChange={e => setVal1(e.target.value)}>
+              <RadioGroup  name="options" value={values.val1} onChange={set('val1')}>
                 <FormControlLabel value="option 1" control={<Radio />} label="yes, I love to" />
                 <FormControlLabel value="option 2" control={<Radio />} label="I often watch it" />
                 <FormControlLabel value="option 3" control={<Radio />} label="No, I  don't like to" />
@@ -98,7 +100,7 @@ function App() {
             <FormField className="wrapper">
             <FormControl>
               <FormLabel>Do you like watching Series at your free time ?</FormLabel>
-              <RadioGroup  name="options" value={val2} onChange={e => setVal2(e.target.value)}>
+              <RadioGroup  name="options" value={values.val2} onChange={set('val2')}>
                 <FormControlLabel value="option 4" control={<Radio />} label="yes, I love to" />
                 <FormControlLabel value="option 5" control={<Radio />} label="I often watch it" />
                 <FormControlLabel value="option 6" control={<Radio />} label="No, I  don't like to" />
@@ -122,7 +124,7 @@ function App() {
             <FormField className="wrapper">
             <FormControl>
               <FormLabel>Do you have any account in Netflix/Prime video/Disney+</FormLabel>
-              <RadioGroup  name="options" value={val} onChange={e => setVal(e.target.value)}>
+              <RadioGroup  name="options" value={values.val3} onChange={set('val3')}>
                 <FormControlLabel value="option 7" control={<Radio />} label="yes, I have an account" />
                 <FormControlLabel value="option 8" control={<Radio />} label="I rent my friends account" />
                 <FormControlLabel value="option 9" control={<Radio />} label="No, I  don't have an account" />
@@ -133,11 +135,17 @@ function App() {
          
 
             <FormField className="wrapper">
-              <Button className="medium ui primary left floated button">Submit</Button>
+              <Button icon className="medium ui primary left floated button">
+                <Icon name='check' />
+                Submit
+              </Button>
               <Button
+                icon
                 className="medium ui right floated grey button"
-                onClick={e => setName('')}
-              >Reset</Button>
+                onClick={e => setValues('')}
+              >
+                <Icon name='x' />RESET
+              </Button>
             </FormField>
 
           </Form>
